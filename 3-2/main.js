3d68992f1e591292d3e63ba54558f9c0daeb84ff
coha150 <cohacoha519@gmail.com>
2020-06-24 13:48:07 +0900

    図形の問題です

---
+++ 3-2/main.js
@@ -0,0 +1,20 @@
+const a = Number(process.argv[2])
+const b = Number(process.argv[3])
+
+for(i=0;i<a;i++){
+    const array = []
+    for(k=0;k<b/2-1;k++){
+        if(i%2===0){
+            array.push("\*"+"-")
+        }else{
+            array.push("-"+"*")
+        }
+
+        if(i%2===0 && k/2%2===1){
+            array.push("*")
+        }else if(i%2===1 &&k/2%2===1){
+            array.push("-")
+        }
+}
+console.log(String(array))
+}
