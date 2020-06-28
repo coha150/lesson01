769a54679489dc3a592112ed1e7666997bf673b0
coha150 <cohacoha519@gmail.com>
2020-06-29 00:55:36 +0900

    4-8  はるこ"の数と"こはる"の数を取得

--- 4-8/main.js
+++ 4-8/main.js
@@ -1,1 +1,12 @@
 const str = "こはるはるここはるここはるはるここはるこはるはるはる"
+let countCo = 0
+let countHa = 0
+for (i = 0; i < str.length; i++) {
+    if (str[i] === "こ" && str[i + 1] === "は" && str[i + 2] === "る") {
+        countCo++
+    } else if (str[i] === "は" && str[i + 1] === "る" && str[i + 2] === "こ") {
+        countHa++
+    }
+}
+console.log(`こはる：${countCo}`)
+console.log(`はるこ：${countHa}`)
