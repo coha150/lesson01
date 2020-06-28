f1db04a5f39f34767819ef1f66dc4da57f363e6b
coha150 <cohacoha519@gmail.com>
2020-06-29 00:24:47 +0900

    3-2 図形作成

--- 3-2/main.js
+++ 3-2/main.js
@@ -1,20 +1,21 @@
 const a = Number(process.argv[2])
 const b = Number(process.argv[3])
 
-for(i=0;i<a;i++){
+for (i = 0; i < a; i++) {
     const array = []
-    for(k=0;k<b/2-1;k++){
-        if(i%2===0){
-            array.push("\*"+"-")
-        }else{
-            array.push("-"+"*")
+    for (k = 0; k < b / 2 - 1; k++) {
+        // 偶数の時
+        if (i % 2 === 0) {
+            array.push("\*" + "-")
+        } else {
+            array.push("-" + "*")
         }
-
-        if(i%2===0 && k/2%2===1){
+        // 奇数の時
+        if (i % 2 === 0 && k / 2 % 2 === 1) {
             array.push("*")
-        }else if(i%2===1 &&k/2%2===1){
+        } else if (i % 2 === 1 && k / 2 % 2 === 1) {
             array.push("-")
         }
-}
-console.log(String(array))
+    }
+    console.log(String(array.join("")))
 }
