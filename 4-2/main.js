1e47f1e3f39ac1f2cbca9034249ed5d0125c7b34
coha150 <cohacoha519@gmail.com>
2020-06-29 00:39:26 +0900

    4-2　1から50までの数字を表示する。3と3の倍数のときだけWOW

---
+++ 4-2/main.js
@@ -0,0 +1,10 @@
+for (i = 1; i <= 50; i++) {
+    if (i % 3 === 0) {
+        console.log("WOW")
+    } else if (String(i).match(/3/)) {
+        console.log("WOW")
+    }
+    else {
+        console.log(i)
+    }
+}
